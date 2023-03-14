44f0a908d0ea176264a2e8cf79aad5d923a8a576
Antony <antony.jacques@laposte.net>
2023-03-14 15:31:44 +0100

    add less prepocessor

--- src/components/shopItemsList.vue
+++ src/components/shopItemsList.vue
@@ -3,57 +3,55 @@
     <h2>list</h2>
     <div class="product-items">
       <div></div>
-      <productItem v-for="product in products" :key="product.id" :product="product"/>
+      <productItem v-for="product in products" :key="product.id" :product="product" />
     </div>
-
   </div>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-import productItem from '@/modules/shop/components/productItem.vue'
-  export default {
-    data(){
-      return {
-
-      }
-    },
-    components: { productItem },
-    computed: {
-      ...mapGetters({
-        products: 'getFilteredProducts',
-      })
-    },
-    beforeMount(){
-      console.log('products before fetch', this.products)
-      if(this.products.length === 0){
-        this.$store.dispatch('loadProducts')
-      }
-      console.log('products after fetch', this.products)
-
+import { mapGetters } from "vuex";
+import productItem from "@/modules/shop/components/productItem.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: { productItem },
+  computed: {
+    ...mapGetters({
+      products: "getFilteredProducts"
+    })
+  },
+  beforeMount() {
+    console.log("products before fetch", this.products);
+    if (this.products.length === 0) {
+      this.$store.dispatch("loadProducts");
     }
+    console.log("products after fetch", this.products);
   }
+};
 </script>
 
-<style>
-  .product-img img{
+<style lang="less">
+.product-img {
+  img {
     width: 250px;
   }
+}
 
-  .product-items{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
+.product-items {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 
-  .product-infos{
-    font-size: 14px;
-    line-height: 18px;
-  }
+.product-infos {
+  font-size: 14px;
+  line-height: 18px;
+}
 
-  .product-card{
-    width: 250px;
-    padding: 1rem;
-  }
+.product-card {
+  width: 250px;
+  padding: 1rem;
+}
 </style>
 
